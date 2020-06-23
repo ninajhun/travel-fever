c9528c4a484ea90b3a7fab0af330567c34f9e8c7
Nina Jhunjhnuwala <ninajhun@berkeley.edu>
2020-06-23 03:26:50 +0000

    add travel fever logo, add container grid, add select user drop down menu

--- client/components/login-page.jsx
+++ client/components/login-page.jsx
@@ -5,12 +5,27 @@ class LoginPage extends React.Component {
   render() {
     return (
 
-      <div className = "login">
-        <div>
-          {/* <img src="images/plane.png" alt="paper airplane logo" /> */}
+      <div className="login">
+        <div className="container">
+
+          <div className="row justify-content-center">
+            <img src="images/plane.png" alt="paper airplane logo" className ="contain"/>
+          </div>
+
+          <div className="row justify-content-center">
+            <h1>Travel Fever</h1>
+          </div>
+
+          <div className="row justify-content-center">
+            <form className="col-10 mt-2">
+              <select className="custom-select custom-select-sm">
+                <option selected>Select User</option>
+              </select>
+
+            </form>
+          </div>
 
         </div>
-        <h1>Travel Fever</h1>
       </div>
 
     );
