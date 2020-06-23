b287e840de3457f1b31feea47bb6f4afec363271
Nina Jhunjhnuwala <ninajhun@berkeley.edu>
2020-06-23 01:04:41 +0000

    add background color, login-page component, create css folder, Travel Fever font

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
+import LoginPage from './login-page';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: null,
-      isLoading: true
+      // message: null,
+      // isLoading: true
     };
   }
 
-  componentDidMount() {
-    fetch('/api/health-check')
-      .then(res => res.json())
-      .then(data => this.setState({ message: data.message || data.error }))
-      .catch(err => this.setState({ message: err.message }))
-      .finally(() => this.setState({ isLoading: false }));
-  }
+  // componentDidMount() {
+
+  // }
 
   render() {
-    return this.state.isLoading
-      ? <h1>Testing connections...</h1>
-      : <h1>{this.state.message.toUpperCase()}</h1>;
+    return (
+      <LoginPage />
+    );
   }
 }
---
+++ client/components/login-page.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class LoginPage extends React.Component {
+
+  render() {
+    return (
+
+      <div className = "login">
+        <div>
+          {/* <img src="images/plane.png" alt="paper airplane logo" /> */}
+
+        </div>
+        <h1>Travel Fever</h1>
+      </div>
+
+    );
+  }
+
+}
+
+export default LoginPage;
