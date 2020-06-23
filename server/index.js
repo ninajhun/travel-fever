0a5a0ed75f45cf3434504bc2dc1fe81a599c7379
Cristian Romero <crisromero02@outlook.com>
2020-06-23 01:10:23 +0000

    Create an express route that GETS all the users and their profile image from the route path

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv/config');
 const express = require('express');
 
-const db = require('./database');
+// const db = require('./database');
 const ClientError = require('./client-error');
 const staticMiddleware = require('./static-middleware');
 const sessionMiddleware = require('./session-middleware');
@@ -13,10 +13,8 @@ app.use(sessionMiddleware);
 
 app.use(express.json());
 
-app.get('/api/health-check', (req, res, next) => {
-  db.query(`select 'successfully connected' as "message"`)
-    .then(result => res.json(result.rows[0]))
-    .catch(err => next(err));
+app.get('/api/users', (req, res, next) => {
+
 });
 
 app.use('/api', (req, res, next) => {
