34e889753322272b1d854f3bcdecdd346f4b5085
Cristian Romero <crisromero02@outlook.com>
2020-06-24 20:11:30 +0000

    Add a styling declation for elements with type listing-icon classname

--- client/components/listing-description.jsx
+++ client/components/listing-description.jsx
@@ -6,12 +6,13 @@ class ListingDescription extends React.Component {
     return (
       <div className="card mt-5">
         <img className="card-img-top" src="" alt=""/>
-        <div className="d-flex align-items-center">
-          <img src={ this.props.profileImg } alt="" className="icon"/>
-          <h6>Cody</h6>
-        </div>
         <div className="card-body">
+          <div className="card-title d-flex align-items-center">
+            <img src={this.props.profileImg} alt="" className="listing-icon" />
+            <h6 className="mb-0 ml-1">Cody</h6>
+          </div>
           <h5 className="card-title">Card title</h5>
+          <h5 className="card-title">Location</h5>
           <p className="card-text">The text in this element is being displayed
           on the view. This text is never going to make sense to any of us. But
           thank you for taking the time out of your day for reading this.</p>
