5fa16d6dae1f9b20f6b6a198111d272c45832eb8
Cristian Romero <crisromero02@outlook.com>
2020-06-25 20:28:15 +0000

    Add a render method in the Button class that creates a react element of type div and contains a classname property

--- client/components/button.jsx
+++ client/components/button.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 
 class Button extends React.Component {
 
+  render() {
+    return (
+      <div>
+        <button type="button" className="uni-button ml-2 mt-1 py-2 px-1">{
+          this.props.text
+        }</button>
+      </div>
+    );
+  }
 }
 
 export default Button;
