077abba5034c4ac543305219da1d83bebac97ea6
Brandon Montes <bmontes115@gmail.com>
2020-06-26 15:29:46 -0700

    add buttons to main page

--- client/components/home-page.jsx
+++ client/components/home-page.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import SearchCity from './search-city';
 import PopularCityList from './popular-city-list';
+import Button from './button';
 
 class HomePage extends React.Component {
 
   render() {
     return (
       <div>
-        <SearchCity />
+        <div>
+          <SearchCity />
+        </div>
+        <div className="d-flex justify-content-center m-2">
+          <div className="mr-3">
+            <Button text="My Listings"/>
+          </div>
+          <div className="ml-3">
+            <Button text="Create Listing"/>
+          </div>
+        </div>
         <PopularCityList/>
       </div>
     );
