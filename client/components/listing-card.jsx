b87f7d2a2df9bc7bd0384baae613f77a508d9d77
Cristian Romero <crisromero02@outlook.com>
2020-06-29 23:11:12 +0000

    Delete extra React Element of type div closing tag from the e listing-card module.

--- client/components/listing-card.jsx
+++ client/components/listing-card.jsx
@@ -28,19 +28,18 @@ class ListingCard extends React.Component {
 
   render() {
     return (
-        <div className='card mb-3 listing' onClick={this.handleClick}>
-          <div className='row no-gutters flex-nowrap card-row'>
-            <div className='col-4'>
-              <img src={this.props.imageUrl} alt={this.props.imageUrl} className='card-img image-listing' />
-            </div>
-            <div className='col-md-8 d-flex align-items-center'>
-              <div className='card-body'>
-                <h5 className='card-title mt-3'>{this.props.title}</h5>
-                <p className='listing-price'>${this.props.price}</p>
-                <div className='fav-heart'>
-                  {this.state.isFavorite ? <i className="fas fa-heart" onClick={this.handleHeartClick} />
-                    : <i className="far fa-heart" onClick={this.handleHeartClick} />}
-                </div>
+      <div className='card mb-3 listing' onClick={this.handleClick}>
+        <div className='row no-gutters flex-nowrap card-row'>
+          <div className='col-4'>
+            <img src={this.props.imageUrl} alt={this.props.imageUrl} className='card-img image-listing' />
+          </div>
+          <div className='col-md-8 d-flex align-items-center'>
+            <div className='card-body'>
+              <h5 className='card-title mt-3'>{this.props.title}</h5>
+              <p className='listing-price'>${this.props.price}</p>
+              <div className='fav-heart'>
+                {this.state.isFavorite ? <i className="fas fa-heart" onClick={this.handleHeartClick} />
+                  : <i className="far fa-heart" onClick={this.handleHeartClick} />}
               </div>
             </div>
           </div>
