115a9435da1c3e522ab4cc6e3276b7e4fc45ccfb
Nina Jhunjhnuwala <ninajhun@berkeley.edu>
2020-06-29 02:18:13 +0000

    add listing card to checkout page

--- client/components/checkout-page.jsx
+++ client/components/checkout-page.jsx
@@ -1,21 +1,45 @@
 import React from 'react';
-import ListingCard from './listing-card';
 
 class CheckoutPage extends React.Component {
-  // constructor(props) {
-  //   super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+      listing: {}
+    };
+  }
 
-  // }
-
-  // getPurchaseListing(){
-  //   fetch
-  // }
+  componentDidMount() {
+    fetch(`/api/listings/${this.props.listingId}`)
+      .then(result => result.json())
+      .then(data => this.setState({
+        listing: data
+      }))
+      .catch(err => console.error(err));
+  }
 
   render() {
     return (
-      <ListingCard listingId={this.props.listingId} />
+      <div className='listing'>
+        <div className='card mb-3' >
+          <div className='row no-gutters flex-nowrap'>
+            <div className='col-md-4'>
+              <img src={this.state.listing.imageUrl} alt={this.state.listing.imageUrl} className='card-img image-listing' />
+            </div>
+            <div className='col-md-8 d-flex align-items-center'>
+              <div className='card-body'>
+                <h5 className='card-title'>{this.state.listing.title}</h5>
+                <p className='listing-price'>${this.state.listing.price}</p>
+                <div className='fav-heart'>
+                  <i className="fas fa-heart" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
+
 }
 
 export default CheckoutPage;
