20e9820344916aa27059a7b466f5c6fe251ae04b
Nina Jhunjhnuwala <ninajhun@berkeley.edu>
2020-06-29 01:30:42 +0000

    fix margin on home page buttons

--- client/components/home-page.jsx
+++ client/components/home-page.jsx
@@ -10,12 +10,12 @@ class HomePage extends React.Component {
         <div className="m-4">
           <SearchCity />
         </div>
-        <div className="d-flex justify-content-center m-2">
-          <div className="mr-3">
-            <button type="button" className="uni-button ml-2 mt-1 py-2 px-1" onClick={() => { this.props.setView('seller-listing-page'); }}>My Listing</button>
+        <div className="d-flex justify-content-around m-2">
+          <div className="">
+            <button type="button" className="uni-button  mt-1 py-2 px-1" onClick={() => { this.props.setView('seller-listing-page'); }}>My Listings</button>
           </div>
-          <div className="ml-3">
-            <button type="button" className="uni-button ml-2 mt-1 py-2 px-1" onClick={() => { this.props.setView('create-listing'); }}>Create Listing</button>
+          <div className="">
+            <button type="button" className="uni-button mt-1 py-2 px-1" onClick={() => { this.props.setView('create-listing'); }}>Create Listing</button>
           </div>
         </div>
         <PopularCityList/>
