ea319f79770b7f9fd362d8d07168711cc373e1d2
Cristian Romero <crisromero02@outlook.com>
2020-06-30 20:16:05 +0000

    Add a react div that has the styling of bootstrap card.

--- client/components/seller-listing-card.jsx
+++ client/components/seller-listing-card.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 
 class SellerListingCard extends React.Component {
 
+  render() {
+    return (
+      <div className='card mb-3 listing' onClick={this.handleClick}>
+        <div className='row no-gutters flex-nowrap card-row'>
+          <div className='col-4'>
+            <img src={this.props.imageUrl} alt={this.props.imageUrl} className='card-img image-listing' />
+          </div>
+          <div className='col-md-8 d-flex align-items-center'>
+            <div className='card-body'>
+              <h5 className='card-title mt-3'>{this.props.title}</h5>
+              <p className='listing-price'>${this.props.price}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SellerListingCard;
