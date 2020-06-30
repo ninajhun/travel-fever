af3a59de9f89ce2145419014586053acddea7c9d
Cristian Romero <crisromero02@outlook.com>
2020-06-30 22:03:07 +0000

    Import the ListingCard component frrom the listing card module. Add a render method to the FavoriteListingsPage class that returns a ListingCard React Element.

--- client/components/favorite-listings-page.jsx
+++ client/components/favorite-listings-page.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import ListingCard from './listing-card';
 
 class FavoriteListingsPage extends React.Component {
 
+  render() {
+    return (
+      <ListingCard />
+    );
+  }
 }
 
 export default FavoriteListingsPage;
