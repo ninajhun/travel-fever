1c725a9c662535992b2fc61cbc125d77237f5212
Cristian Romero <crisromero02@outlook.com>
2020-07-01 04:31:49 +0000

    Add a react element of type icon that has a bootstrap classname for a heart. Add an onClick property to it that is assgined to a JavaScript expression that runs an anonymous callback function that runs the setView method of the props object of this objectand passes in a string of favorites-page.

--- client/components/bottom-nav-bar.jsx
+++ client/components/bottom-nav-bar.jsx
@@ -7,8 +7,13 @@ class BottomNavBar extends React.Component {
       <div className="container h-45">
         <nav className="navbar fixed-bottom border-top justify-content-around">
           <i className="fas fa-home fa-lg" onClick={() => this.props.setView('home', {})}></i>
-          {/* <i className="far fa-heart fa-lg"></i> */}
-          <i className="fas fa-search fa-lg" onClick={() => { this.props.setView('listings-page', {}); this.props.getCustomerListings(); } }></i>
+          <i className="far fa-heart fa-lg"
+            onClick={() => this.props.setView('favorites-page')}></i>
+          <i className="fas fa-search fa-lg"
+            onClick={() => {
+              this.props.setView('listings-page', {});
+              this.props.getCustomerListings();
+            } }></i>
           {/* <i className="far fa-envelope fa-lg"></i> */}
         </nav>
 
