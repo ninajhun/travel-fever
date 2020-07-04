ce70741bf46bcc76c01c55a7ae40813d63aa5ea6
Nina Jhunjhnuwala <ninajhun@berkeley.edu>
2020-07-04 19:45:17 +0000

    refactor PopularCity List to pass location as props

--- client/components/popular-city-list.jsx
+++ client/components/popular-city-list.jsx
@@ -1,47 +1,23 @@
 import React from 'react';
 import PopularCityListing from './popular-city-listing';
 
-class PopularCityList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      locations: []
-    };
-    this.getLocations = this.getLocations.bind(this);
-  }
-
-  componentDidMount() {
-    this.getLocations();
-  }
-
-  getLocations() {
-    fetch('/api/locations')
-      .then(response => response.json())
-      .then(data => this.setState({
-        locations: data
-      }));
-  }
-
-  render() {
-
-    return (
-      <div className="container mt-3">
-        <div className="row">
-          <h5 className="ml-3">Popular Cities</h5>
-        </div>
-
-        <div className="row justify-content-center">
-          {
-            this.state.locations.map(location => {
-              return <PopularCityListing key={location.locationId} imageUrl={location.imageUrl} locationId={location.locationId} name={location.name} setView={this.props.setView} getCustomerListings={this.props.getCustomerListings} />;
-            })
-          }
-        </div>
-
+function PopularCityList(props) {
+  return (
+    <div className="container mt-3">
+      <div className="row">
+        <h5 className="ml-3">Popular Cities</h5>
       </div>
 
-    );
-  }
+      <div className="row justify-content-center">
+        {
+          props.locations.map(location => {
+            return <PopularCityListing key={location.locationId} imageUrl={location.imageUrl} locationId={location.locationId} name={location.name} setView={props.setView} getCustomerListings={props.getCustomerListings} />;
+          })
+        }
+      </div>
+
+    </div>
+  );
 }
 
 export default PopularCityList;
