1d0406ef9c37dfa5831c108ea4fd62da32b75c39
Nina Jhunjhnuwala <ninajhun@berkeley.edu>
2020-07-04 20:42:01 +0000

    add isModalOpen to state and skeleton for delete confirmation pop-up

--- client/components/seller-listing-description.jsx
+++ client/components/seller-listing-description.jsx
@@ -4,8 +4,15 @@ import ListingDescription from './listing-description';
 class SellerListingDescription extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isModalOpen: true
+    };
     this.returnMyListings = this.returnMyListings.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleDeleteModal() {
+
   }
 
   handleDelete() {
@@ -19,6 +26,37 @@ class SellerListingDescription extends React.Component {
   }
 
   render() {
+
+    if (this.state.isModalOpen) {
+      return (
+        <div>
+          <div className="card delete-card">
+            <div className="card-body">
+              <h5 className="card-title">Are you sure you want to delete?</h5>
+              <a href="#" className="btn btn-primary">Cancel</a>
+              <a href="#" className="btn btn-primary">Delete</a>
+            </div>
+          </div>
+
+          <div>
+            <ListingDescription listingId={this.props.listingId}
+              setView={this.props.setView}
+            />
+          </div>
+          <div className="d-flex justify-content-around my-2">
+            <div>
+              <button type="button" className="uni-button mt-1 py-2 px-1" onClick={this.returnMyListings}>Confirm</button>
+            </div>
+            <div>
+              <button type="button" className="uni-button mt-1 py-2 px-1" onClick={this.handleDelete}>Delete</button>
+            </div>
+          </div>
+
+        </div>
+      );
+    }
+
+    // if (!this.state.isModalOpen) {
     return (
       <div>
         <div>
@@ -36,6 +74,9 @@ class SellerListingDescription extends React.Component {
         </div>
       </div>
     );
+
+    // }
+
   }
 }
 
