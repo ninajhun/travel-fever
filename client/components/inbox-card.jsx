8bec5a72991ce45a744a28ff4a9f8302d3b4263e
Nina Jhunjhnuwala <ninajhun@berkeley.edu>
2020-12-03 21:06:51 +0000

    changed inbox from cards to rows

--- client/components/inbox-card.jsx
+++ client/components/inbox-card.jsx
@@ -4,21 +4,23 @@ function inboxCard(props) {
 
   return (
     <div>
-      <div className='card inbox' onClick={() => { props.setView('messages', { recipientImg: props.imageUrl, recipientId: props.recipientId, chatId: props.chatId }); props.getMessages(props.chatId); }}>
-        <div className='d-flex'>
-          <div className='inbox-icon-container'>
-            <img src={props.imageUrl} alt={props.imageUrl} className='card-img inbox-icon' />
-          </div>
-          <div className='col-md-8 d-flex align-items-center'>
-            <div className='card-body p-3'>
-              <p className='card-title mt-3' style={{ marginLeft: 30 }}>Connect with {props.username}!</p>
-            </div>
+      <div className='row p-2' onClick={() => { props.setView('messages', { recipientImg: props.imageUrl, recipientId: props.recipientId, chatId: props.chatId }); props.getMessages(props.chatId); }}>
+
+        <div className='col-2 d-flex align-content-center justify-content-center'>
+          <img src={props.imageUrl} alt={props.imageUrl} className='inbox-icon' />
+        </div>
+
+        <div className='col-10'>
+          <div>
+            <p className='mt-3'>Connect with {props.username}!</p>
           </div>
         </div>
+
       </div>
       <div className="d-flex justify-content-center">
         <div className="inbox-border"></div>
       </div>
+
     </div>
   );
 }
